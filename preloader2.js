a0b00692f6a2a691df5f3296c12d07a93f8ae188
AirTeamAi谢毅 <qc_6c4ef72f6d274905ba0c2952724fe968@git.code.tencent.com>
2020-02-16 10:48:59 +0800

    Preloader update

--- preloader2.js
+++ preloader2.js
@@ -1,20 +1,10 @@
-function Preloader(){
-    return {
-        data:{},
-        preloadDatas:{},
-        preloadByConfigure:function(templateURL,jQSelector){
-                $.ajax({'url':templateURL,'success':function(ret){
-                    var reg = new RegExp("\{\{(.+?)\}\}","igm");
-                    ret.replace(reg, function (node, key) {
-                              return this.data[key];
+function addLog(param){
+		tpl='<article class="excerpt {{excerpt}} wow fadeInUp" style="padding: 10px 10px 10px 20px;"><img data-src="{{log_thumb}}" class="autoload-pics thumb" ></a><header><a class="cat" href="{{sortURL}}>{{sortAttr}}</a><a class="focus" href="{{log_url}}"><h2><a href="{{log_url}}" title="{{title}}">{{title}}</a></h2></header><p class="note">{{logdes}}</p><p class="meta"><time><i class="fa fa-clock-o"></i>{{stime}}</time><span class="pv"><i class="fa fa-eye"></i>阅读（{{reads}}）</span><a class="pc" href="{{log_url}}#comments"><i class="fa fa-comments-o"></i>评论({{comments}})</a></p></article>';
+					var reg = new RegExp("\{\{(.+?)\}\}","igm");
+					var data2=param;
+                    tpl=tpl.replace(reg, function (node, key) {
+                              return data2[key];
                     });
-                    jQSelector.append(ret);
-                }
-            
-            });
-            
-        }
-        
-        
-    }
-}
+		$("#log_list_autoload").append(tpl);
+	}
+function Preloader(){return{data:{},preloadDatas:{},preloadByConfigure:function(templateURL,jQSelector){var that=this;$.ajax({'url':templateURL,'success':function(ret){var reg=new RegExp("\{\{(.+?)\}\}","igm");ret=ret.replace(reg,function(node,key){console.log(key,that.data[key]);return that.data[key]});jQSelector.append(ret)}})}}}var preloader=new Preloader();preloader.data={'logincode':'<?=Option::get('login_code')?>','CDN_URI':'<?=CDN_URI?>','navhide':'<?=$navhide?>','pjax':'<?=$pjax?>','down_next':'<?=$down_next?>','webcompress':'<?=$webcompress?>','BLOG_URL':'<?=BLOG_URL?>','site_title':'<?=$site_title?>','site_description':'<?=$site_description?>'};preloader.preloadByConfigure("<?=CDN_URI?>head.autoload.jhtml",$("head"));preloader.preloadByConfigure("<?=CDN_URI?>footer.autoload.jhtml",$("body"));function loadImage(jQSelector){var x=jQSelector;x.attr("src",x.attr('data-src'))}
